a0ba3505549f3bb02a11ed8587d69d6c1bbfa88f
SWarringer <warringer.simon@gmail.com>
2026-02-23 18:59:16 +0100

    working on this bs

--- components/mpu6050/include/mpu6050.h
+++ components/mpu6050/include/mpu6050.h
@@ -12,3 +12,11 @@ void mpu6050_init(i2c_master_dev_handle_t dev);
 esp_err_t mpu6050_read_accel(i2c_master_dev_handle_t dev, mpu_vec3_t *accel);
 esp_err_t mpu6050_read_gyro(i2c_master_dev_handle_t dev, mpu_vec3_t *gyro);
 esp_err_t mpu6050_check_and_wake(i2c_master_dev_handle_t dev);
+esp_err_t i2c_write_reg(i2c_master_dev_handle_t dev,
+                        uint8_t reg,
+                        uint8_t value);
+
+esp_err_t i2c_read_reg(i2c_master_dev_handle_t dev,
+                       uint8_t reg,
+                       uint8_t *data,
+                       size_t len);
